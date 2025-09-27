a362bc517daaf1c50221a7ccffcdda8fe3662185
quyenlshb <dragonfly.bhb@gmail.com>
2025-09-28 01:56:19 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
 // src/App.tsx
 import { Routes, Route } from 'react-router-dom'
 
-// Components
+// 1. IMPORT COMPONENTS CHÍNH
 import Shell from './components/Shell'
+import HomePage from './pages/HomePage'
+import LearningMode from './components/LearningMode'
+import VocabularyManager from './components/VocabularyManager'
+import StatisticsPage from './pages/StatisticsPage'
+import SettingsPage from './pages/SettingsPage'
+
+// 2. IMPORT AUTH
 import { AuthProvider, useAuthProvider } from './components/AuthProvider'
+
+// 3. IMPORT TOASTER
 import { Toaster } from './components/ui/toaster'
 
-// Pages
-import HomePage from './pages/Home'
-import LearningMode from './components/LearningMode'
-import VocabularyPage from './pages/Vocabulary'
-import StatisticsPage from './pages/Statistics'
-import SettingsPage from './pages/Settings'
-
-// Lấy context auth
-const authContext = useAuthProvider()
-
 export default function App() {
+  const authContext = useAuthProvider()
+
   return (
     <AuthProvider value={authContext}>
-      <Toaster />
+      <Toaster /> {/* Phải có Toaster nếu dùng useToast */}
       <Routes>
         <Route path="/" element={<Shell />}>
+          {/* Trang chính */}
           <Route index element={<HomePage />} />
+
+          {/* Protected routes */}
           <Route path="learn" element={<LearningMode />} />
-          <Route path="vocabulary" element={<VocabularyPage />} />
+          <Route path="vocabulary" element={<VocabularyManager />} />
           <Route path="statistics" element={<StatisticsPage />} />
           <Route path="settings" element={<SettingsPage />} />
+          
+          {/* Fallback */}
+          <Route path="*" element={<HomePage />} />
         </Route>
       </Routes>
     </AuthProvider>
