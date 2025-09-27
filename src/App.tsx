6255580fb032c77f1f2843c92f1ae7e977ad9e26
quyenlshb <dragonfly.bhb@gmail.com>
2025-09-28 01:50:22 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,34 @@
-// File: src/App.tsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// src/App.tsx
+import { Routes, Route } from 'react-router-dom'
+
+// Components
 import Shell from './components/Shell'
 import { AuthProvider, useAuthProvider } from './components/AuthProvider'
-import AuthModal from './components/AuthProvider/AuthModal'
 import { Toaster } from './components/ui/toaster'
+
+// Pages
 import HomePage from './pages/Home'
-import LearnPage from './pages/Learn'
+import LearningMode from './components/LearningMode'
 import VocabularyPage from './pages/Vocabulary'
 import StatisticsPage from './pages/Statistics'
 import SettingsPage from './pages/Settings'
 
-export default function App() {
-  // Lấy context auth
-  const authContext = useAuthProvider()
+// Lấy context auth
+const authContext = useAuthProvider()
 
+export default function App() {
   return (
     <AuthProvider value={authContext}>
-      <Router>
-        {/* Modal auth */}
-        <AuthModal />
-
-        {/* Toaster chung */}
-        <Toaster />
-
-        {/* Routes */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/app/*" element={<Shell />}>
-            <Route path="learn" element={<LearnPage />} />
-            <Route path="vocabulary" element={<VocabularyPage />} />
-            <Route path="statistics" element={<StatisticsPage />} />
-            <Route path="settings" element={<SettingsPage />} />
-          </Route>
-        </Routes>
-      </Router>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Shell />}>
+          <Route index element={<HomePage />} />
+          <Route path="learn" element={<LearningMode />} />
+          <Route path="vocabulary" element={<VocabularyPage />} />
+          <Route path="statistics" element={<StatisticsPage />} />
+          <Route path="settings" element={<SettingsPage />} />
+        </Route>
+      </Routes>
     </AuthProvider>
   )
 }
