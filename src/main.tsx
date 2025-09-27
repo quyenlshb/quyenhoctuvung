48462815bbaf259ccd0c6647ebd04cf890cd8c47
quyenlshb <dragonfly.bhb@gmail.com>
2025-09-28 03:00:00 +0700

    b5

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./shadcn.css"; // Đúng đường dẫn
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './shadcn.css' // đường dẫn chính xác
+import { Home } from './pages/Home'
+import { Settings } from './pages/Settings'
+import { Statistics } from './pages/Statistics'
+import { AuthProvider } from './components/AuthProvider'
 
-const container = document.getElementById("root");
-const root = createRoot(container!);
+const container = document.getElementById('root')
+if (!container) throw new Error('Root container missing in index.html')
 
+const root = createRoot(container)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/statistics" element={<Statistics />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
-);
+)
